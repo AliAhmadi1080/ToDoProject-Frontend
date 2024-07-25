97a68e525457bf7ea33b776fa5d29334924047a4
Ali <aliahmadi.xx90@gmail.comOmit>
2024-07-25 12:03:45 +0330

    I change value of userInfoContext context

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { useState } from "react";
+import Navbar from "./components/Navbar";
+import userInfoContext from "./contexts/UserInfoContext";
+
 function App() {
-  return <></>
+  const [ usernameInfo, setUsernameInfo ] = useState({'username':'علی','isauth':true});
+
+  return (
+    <userInfoContext.Provider value={usernameInfo}>
+      <Navbar />
+    </userInfoContext.Provider>
+  );
 }
 
 export default App;
