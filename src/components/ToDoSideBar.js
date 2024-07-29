df5e739099725623b9943e45f60dfc639e4d0a01
Ali <aliahmadi.xx90@gmail.comOmit>
2024-07-29 10:15:45 +0330

    I change styles for first div.

--- src/components/ToDoSideBar.js
+++ src/components/ToDoSideBar.js
@@ -5,7 +5,7 @@ export default function ToDoSideBar() {
   const ToDolist = [{ title: "خانه", todos: ["ظرف", "ماشین"] }];
   const Taglist = [{ title: "زندگی" }, { title: "life" }];
   return (
-    <div className="text-left px-7 text-lg w-40 ml-2 bg-red-50 mr-auto max-h-[40rem] overflow-y-auto">
+    <div className="text-left px-7 text-lg w-40 ml-2 bg-red-50 max-h-[40rem] overflow-y-auto">
       <h3 className=" text-xl text-center border-b-2 border-black">کار ها</h3>
       {ToDolist.map((item, index) => (
         <ToDoListContainer key={index} title={item.title} todos={item.todos} />
