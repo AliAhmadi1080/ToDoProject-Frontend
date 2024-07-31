d264d0a400c50cb3616efea1ae0f0e5ae247ebd3
Ali <aliahmadi.xx90@gmail.comOmit>
2024-07-31 09:48:13 +0330

    I add "type"  property to component for set input type.

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,8 +1,8 @@
-export default function Input({lable}) {
+export default function Input({lable, type='text'}) {
   return (
     <div className="flex flex-col my-5">
       <label className="mr-6 w mb-2">{lable}</label>
-      <input className="mx-6 w-[80%] px-3 bg-gray-100 rounded-lg"/>
+      <input type={type} className="mx-6 w-[80%] px-3 bg-gray-100 rounded-lg"/>
     </div>
   );
 }
