eafd4c0398db3fa2ea02477d6c61e12b11bc4d9e
Ali <aliahmadi.xx90@gmail.comOmit>
2024-08-03 09:58:44 +0330

    I create login logics. get access and refresh token then save these.

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,8 +1,8 @@
-export default function Input({lable, type='text'}) {
+export default function Input({lable, type='text', onchange}) {
   return (
     <div className="flex flex-col my-5">
       <label className="mr-6 w mb-2">{lable}</label>
-      <input type={type} className="mx-6 w-[80%] px-3 bg-gray-100 rounded-lg"/>
+      <input onChange={onchange} type={type} className="mx-6 w-[80%] px-3 bg-gray-100 rounded-lg"/>
     </div>
   );
 }
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,26 +1,74 @@
+import { useState } from "react";
+import { redirect } from "react-router-dom";
 import Input from "./Input";
 
 export default function LoginForm() {
+  const [isLogin, setIsLogin] = useState(localStorage.getItem("islogin"));
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const emailChangeHandler = (e) => {
+    setEmail(e.target.value);
+  };
+  const passwordChangeHandler = (e) => {
+    setPassword(e.target.value);
+  };
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    const response = await fetch("http://localhost:8000/api-auth/token/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    });
+
+    const data = await response.json();
+    if (response.ok) {
+      setIsLogin(true);
+      localStorage.setItem("islogin", "true");
+      localStorage.setItem('refresh',data['refresh'])
+      localStorage.setItem('access',data['access'])
+    } else {
+      console.error(data.message);
+    }
+  };
   return (
     <>
       <div className="pb-5 mt-[8rem] shadow-xl">
-        <Input lable={"ایمیل"} />
-        <Input lable={"رمز عبور"} />
-        <div className="font-bold w-12 mx-auto cursor-pointer rounded-full flex justify-center items-center bg-red-500 text-3xl  h-12 text-center ">
-          G{" "}
-        </div>
-        <div className="flex justify-end mx-14">
-          <button
-            style={{ background: "#0d6efd" }}
-            className="p-2 rounded-lg text-white"
-            onClick={() => {
-              console.log("you think submited");
-            }}
-          >
-            ارسال
-          </button>
-        </div>
+        <form onSubmit={submitHandler}>
+          <Input onchange={emailChangeHandler} type="email" lable={"ایمیل"} />
+          <Input
+            onchange={passwordChangeHandler}
+            type="password"
+            lable={"رمز عبور"}
+          />
+          <div className="font-bold w-12 mx-auto cursor-pointer rounded-full flex justify-center items-center bg-red-500 text-3xl  h-12 text-center ">
+            G{" "}
+          </div>
+          <div className="flex justify-end mx-14">
+            <button
+              style={{ background: "#0d6efd" }}
+              className="p-2 rounded-lg text-white"
+              onClick={() => {
+                console.log("you think submited");
+              }}
+            >
+              ارسال
+            </button>
+          </div>
+        </form>
       </div>
+      {isLogin ? (
+        <div
+          className="bg-blue-100 border-t border-b border-blue-500 text-blue-700 px-4 py-3"
+          role="alert"
+        >
+          <p className="font-bold">پیام</p>
+          <p className="text-sm">شما با موفقیت وارد شدید</p>
+        </div>
+      ) : (
+        <></>
+      )}
     </>
   );
 }
