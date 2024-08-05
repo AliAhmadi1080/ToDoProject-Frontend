55698e8a69a61d126944f505ec69b1ba3814d9d3
Ali <aliahmadi.xx90@gmail.comOmit>
2024-08-05 09:33:47 +0330

    I change login form.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -57,7 +57,7 @@ export default function LoginForm() {
           </div>
         </form>
       </div>
-      {isLogin ? (
+      {isLogin=='true' ? (
         <div
           className="bg-blue-100 border-t border-b border-blue-500 text-blue-700 px-4 py-3"
           role="alert"
