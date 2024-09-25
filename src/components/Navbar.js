f223309d297dea35b1416d79f61ba7d04b1ab7ad
Ali <aliahmadi.xx90@gmail.comOmit>
2024-09-25 16:06:56 +0330

    I add some path for create todo, tag, status and todolist.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,58 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import userInfoContext from "../contexts/UserInfoContext";
 import NavbarItem from "./NavbarItem";
-
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   const userInfo = useContext(userInfoContext).user;
+  const [showToDoItems, setShowToDoItems] = useState(false);
 
   return (
-    <div className="flex justify-start border  rounded-xl">
+    <div className="flex justify-start border rounded-xl">
       {userInfo.isauth ? (
         <>
           <NavbarItem text="خانه" />
           <NavbarItem
             text={
-              <>
-
-              </>
+              <div
+                onMouseOver={() => {
+                  setShowToDoItems(true);
+                }}
+                onMouseOut={() => {
+                  setShowToDoItems(false);
+                }}
+              >
+                <p>todo ها</p>
+                {showToDoItems ? (
+                  <div
+                    id="myDropdown"
+                    style={{
+                      display: "block",
+                      position: "absolute",
+                      backgroundColor: "#f1f1f1",
+                      minWidth: "160px",
+                      overflow: "auto",
+                      zIndex: "1",
+                    }}
+                    className=" shadow-xl flex flex-col"
+                  >
+                    <Link to={`/todo/create`}>
+                      <div className="block">ایجاد todo</div>
+                    </Link>
+                    <Link to={`/tag/create`}>
+                      <div className="block">ایجاد تگ</div>
+                    </Link>
+                    <Link to={`/status/create`}>
+                      <div className="block">ایجاد وضعیت</div>
+                    </Link>
+                    <Link to={`/todolist/create`}>
+                      <div className="block">ایجاد لیست todo</div>
+                    </Link>
+                  </div>
+                ) : (
+                  <></>
+                )}
+              </div>
             }
           />
           <NavbarItem
