edd3b86e96181143ff94d86cc2ab0df09e749d23
Ali <aliahmadi.xx90@gmail.comOmit>
2024-09-25 17:12:57 +0330

    I add a select for edit todo.

--- src/components/ToDoEditContainer.js
+++ src/components/ToDoEditContainer.js
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import useChangeAccessToken from "../hooks/useChangeAccessToken";
 import { useEffect, useState } from "react";
 
@@ -6,15 +6,19 @@ export default function ToDoEditContainer() {
   const [toDo, setToDo] = useState({});
   const [tagList, setTagList] = useState([]);
   const [status, setStatus] = useState();
+  const [toDoList, setToDoList] = useState([]);
 
   const [title, setTitle] = useState();
   const [subtitle, setSubtitle] = useState();
   const [description, setDescription] = useState();
   const [inputStatus, setInputStatus] = useState();
   const [inputTagList, setInputTagList] = useState();
+  const [inputToDoList, setInputToDoList] = useState();
 
   const [getAccessToken] = useChangeAccessToken();
   const { todoId } = useParams();
+  const navigate = useNavigate();
+
   const getToDo = async () => {
     const response = await fetch("http://localhost:8000/todo/" + todoId + "/", {
       method: "GET",
@@ -90,16 +94,38 @@ export default function ToDoEditContainer() {
         description: description,
         status: inputStatus,
         tags: inputTagList,
+        todo_list: inputToDoList,
       }),
     });
   };
+
+  const getToDoList = async () => {
+    const response = await fetch("http://localhost:8000/todo/todolist", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + localStorage.getItem("access"),
+      },
+    });
+
+    const data = await response.json();
+
+    if (data["code"] === "token_not_valid") {
+      getAccessToken();
+    } else {
+      setToDoList(data);
+    }
+  };
+
   useEffect(() => {
     getToDo();
     getStatus();
   }, [todoId]);
   useEffect(() => {
     getTag();
-  }, [toDo]);
+    getToDoList();
+    setInputToDoList(toDoList[0]?.id)
+  }, [toDo, status]);
 
   return (
     <div
@@ -180,11 +206,39 @@ export default function ToDoEditContainer() {
             </label>
           </div>
         </div>
+        <div className=" col-span-3 py-6 flex">
+          <label className="mt-2 mx-3">انتخاب لیست:</label>
+          <div className="relative h-10 w-72 min-w-[200px]">
+            {/* material tailwind */}
+
+            <select
+              onChange={(event) => setInputToDoList(event.target.value)}
+              className="peer h-full w-full rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent pr-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 empty:!bg-gray-900 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+            >
+              {toDoList?.map((item, index) =>
+                item.id === toDo.todo_list ? (
+                  <option key={index} selected value={item.id}>
+                    {item.name}
+                  </option>
+                ) : (
+                  <option key={index} value={item.id}>
+                    {item.name}
+                  </option>
+                )
+              )}
+            </select>
+            <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-gray-900 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-gray-900 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+              انتخاب لیست
+            </label>
+          </div>
+        </div>
       </div>
       <div dir="ltr" className=" flex justify-around">
         <button
           className=" bg-emerald-500 w-full text-white p-2 rounded-l-full hover:shadow-2xl hover:bg-emerald-600"
-          onClick={() => {}}
+          onClick={() => {
+            navigate(`/todo/${todoId}`);
+          }}
         >
           بیخیال
         </button>
