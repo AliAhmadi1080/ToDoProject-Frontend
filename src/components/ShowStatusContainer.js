56dc93fe749fc105e314c8488a9ee94f61c3ef9c
Ali <aliahmadi.xx90@gmail.comOmit>
2024-10-14 19:28:31 +0330

    I add delete button for Status and Tag.

--- src/components/ShowStatusContainer.js
+++ src/components/ShowStatusContainer.js
@@ -1,10 +1,15 @@
 import { useState } from "react";
 import useChangeAccessToken from "../hooks/useChangeAccessToken";
 import { useEffect } from "react";
+import DangerModal from "./DangerModal";
 
 export default function ShowStatusContainer() {
   const [status, setStatus] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+  const [statusId, setStatusId] = useState();
+
   const getAccessToken = useChangeAccessToken();
+
   const getStatus = async () => {
     const response = await fetch("http://localhost:8000/todo/status", {
       method: "GET",
@@ -31,10 +36,33 @@ export default function ShowStatusContainer() {
         className=" bg-slate-50 flex flex-col w-2/5 h-fit mx-auto mt-24 p-3"
       >
         {status.map((value, index) => (
-          <div index={index} className="mb-5">
-            {value.name}
+          <div key={index} className="mb-5 flex justify-between">
+            <p>{value.name}</p>
+            <button
+              onClick={() => {
+                setShowModal(true);
+                setStatusId(value.id);
+              }}
+              className="bg-red-500 rounded-xl text-white p-1"
+            >
+              حذف
+            </button>
           </div>
         ))}
+      </div>
+      <div dir="rtl">
+        {showModal ? (
+          <DangerModal
+            title={"حذف وضعیت"}
+            text={"آیا شما برای حذف این وضعیت مطمعا هستید؟"}
+            isOpen={showModal}
+            setIsOpen={setShowModal}
+            id={statusId}
+            path={`http://localhost:8000/todo/status/${statusId}/`}
+          />
+        ) : (
+          <></>
+        )}
       </div>
     </>
   );
--- src/components/ShowTagContainer.js
+++ src/components/ShowTagContainer.js
@@ -1,10 +1,15 @@
 import { useState } from "react";
 import useChangeAccessToken from "../hooks/useChangeAccessToken";
 import { useEffect } from "react";
+import DangerModal from "./DangerModal";
 
 export default function ShowTagContainer() {
   const [tag, setTag] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+  const [tagId,setTagId] = useState()
+
   const getAccessToken = useChangeAccessToken();
+
   const getTag = async () => {
     const response = await fetch("http://localhost:8000/todo/tag", {
       method: "GET",
@@ -31,10 +36,31 @@ export default function ShowTagContainer() {
         className=" bg-slate-50 flex flex-col w-2/5 h-fit mx-auto mt-24 p-3"
       >
         {tag.map((value, index) => (
-          <div index={index} className="mb-5">
-            {value.name}
+          <div key={index} className="mb-5 flex justify-between">
+            <p>{value.name}</p>
+            <button
+              onClick={() => {setShowModal(true);setTagId(value.id)}}
+              className="bg-red-500 rounded-xl text-white p-1"
+            >
+              حذف
+            </button>
           </div>
         ))}
+      </div>
+      
+      <div dir="rtl">
+        {showModal ? (
+          <DangerModal
+            title={"حذف تگ"}
+            text={"آیا شما برای حذف این تگ مطمعا هستید؟"}
+            isOpen={showModal}
+            setIsOpen={setShowModal}
+            id={tagId}
+            path={`http://localhost:8000/todo/tag/${tagId}/`}
+          />
+        ) : (
+          <></>
+        )}
       </div>
     </>
   );
